b1c1654c08ec210a82dc275f62449eba941e322e
Jorge R <jrubiosantiago@gmail.com>
2022-03-23 14:30:09 +0100

    Perfil

---
+++ JS/perfil.js
@@ -0,0 +1,37 @@
+function cambioColor()
+{
+    var color1 = document.getElementById("miColor1").value
+    var rgbColor1 = document.getElementById("color1");
+
+    var color2 = document.getElementById("miColor2").value
+    var rgbColor2 = document.getElementById("color2");
+
+
+    rgbColor1.innerHTML = color1;
+    rgbColor2.innerHTML = color2;
+
+    var fondo = document.getElementById("fondo");
+
+    fondo.style.background = 'linear-gradient(' + color1 + ', ' + color2 + ')';
+}
+function guardarCambios()
+{
+    var pantallaEmergente = document.getElementById("pantallaEmergente");
+    var imagenPerfil = document.getElementById("imagenPerfil");
+    var color1 = document.getElementById("miColor1").value
+    
+
+    var color2 = document.getElementById("miColor2").value
+    
+    imagenPerfil.style.background = 'linear-gradient(' + color1 + ', ' + color2 + ')';
+    pantallaEmergente.style.visibility  = "hidden";
+    
+
+}
+function pantallaEmergente()
+{
+    
+    var pantallaEmergente = document.getElementById("pantallaEmergente");
+    pantallaEmergente.style.visibility  = "visible";
+    
+}
