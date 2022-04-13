56067d30045f45a043b83549ecb0a6b6f3ef9468
Gonzalo <goloro02@gmail.com>
2022-04-13 19:43:11 +0200

    Registro

--- JS/users.js
+++ JS/users.js
@@ -6,9 +6,8 @@ const RequestHandler = new RequestHandlerClass()
 
 const gridLogin = document.querySelector(".form-login")
 const gridRegistro = document.querySelector(".form-registro")
-const btnInicioSesionForm = document.querySelector("#inicioSesionForm")
-const btnRegistroForm = document.querySelector("#registroForm")
-
+const btnInicioSesionForm = document.getElementById("inicioSesionForm")
+const btnRegistroForm = document.getElementById("registroForm")
 
 // EVENT LISTENERS
 document.getElementById("btnLogin").addEventListener("click", e => {
@@ -19,58 +18,86 @@ document.getElementById("btnRegistro").addEventListener("click", e => {
     e.preventDefault()
     registro()
 })
+btnInicioSesionForm.addEventListener("click", e => {
+    e.preventDefault()
+    inicioSesionForm()
+})
+btnRegistroForm.addEventListener("click", e => {
+    e.preventDefault()
+    registroForm()
+})
 
 // FUNCTIONS
+const localUser = localStorage.getItem('Means_userLogued')
+if (localUser) window.open("/HTML/app.html", "_self")
+
+const localOption = localStorage.getItem('Means_userOption')
+if (localOption == "registro") registroForm()
+localStorage.removeItem("Means_userOption")
+
 async function login() {
-    const email = document.getElementById("campoEmailL").value
-    const password = document.getElementById("campoPasswordL").value
+    const email = document.getElementById("campoEmailL")
+    const password = document.getElementById("campoPasswordL")
 
-    const existUser = await RequestHandler.getDefault("http://localhost:8085/users/" + email + "/" + password)
+    const existUser = await RequestHandler.getDefault("http://localhost:8085/users/" + email.value + "/" + password.value)
 
-    if (existUser) {alert("Users exists")}
+    email.value = ""
+    password.value = ""
+
+    if (existUser) {
+        localStorage.setItem('Means_userLogued', JSON.stringify(existUser))
+        window.open("/HTML/app.html", "_self")
+    }
+}
+
+async function registro() {
+    const name = document.getElementById("campoNameR")
+    const email = document.getElementById("campoEmailR")
+    const phone = document.getElementById("campoNameR")
+    const password = document.getElementById("campoPasswordR")
+
+    const data = {
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        password: password.value
+    }
+
+    const registUser = await RequestHandler.postDefault("http://localhost:8085/users/register", data)
+
+    name.value = ""
+    email.value = ""
+    phone.value = ""
+    password.value = ""
+
+    if (registUser == "Usuario registrado con éxito") {
+        inicioSesionForm()
+    }
 }
 
 function inicioSesionForm(){
-    //querySelector() Devuelve el primer elemento del documento
-    const gridLogin = document.querySelector(".form-login");
-    const gridRegistro = document.querySelector(".form-registro");
-    const btnInicioSesionForm = document.querySelector("#inicioSesion");
-    const btnRegistroForm = document.querySelector("#registro");
-
     //Superponer login
     gridRegistro.style.display = "none";
     gridLogin.style.display = "block";
 
-    //ini= background: transparente, color: black
-    //regis= backgro: azul, color: white
-    btnInicioSesion.style.backgroundColor= "transparent";
-    btnInicioSesion.style.color="black";
+    btnInicioSesionForm.style.backgroundColor= "transparent";
+    btnInicioSesionForm.style.color="black";
     
-    btnRegistro.style.backgroundColor= "#1641EA";
-    btnRegistro.style.color="white";
-
-    
+    btnRegistroForm.style.backgroundColor= "#1641EA";
+    btnRegistroForm.style.color="white";
 }
 
 function registroForm(){
-    const gridLogin = document.querySelector(".form-login");
-    const gridRegistro = document.querySelector(".form-registro");
-
     //Superponer Registro 
     gridLogin.style.display = "none";
     gridRegistro.style.display = "block";
-
-     //ini= background: azul, color: white
-    //regis= backgro: transpa, color: black
-    const btnInicioSesionForm = document.querySelector("#inicioSesion");
-    const btnRegistroForm = document.querySelector("#registro");
     
-    btnInicioSesion.style.backgroundColor= "#1641EA";
-    btnInicioSesion.style.color="white";
-    btnInicioSesion.style.borderTopLeftRadius = "6px";
-    btnInicioSesion.style.borderBottomRightRadius = "40px";
+    btnInicioSesionForm.style.backgroundColor= "#1641EA";
+    btnInicioSesionForm.style.color="white";
+    btnInicioSesionForm.style.borderTopLeftRadius = "6px";
+    btnInicioSesionForm.style.borderBottomRightRadius = "40px";
 
-    btnRegistro.style.backgroundColor = "transparent";
-    btnRegistro.style.color= "black";
+    btnRegistroForm.style.backgroundColor = "transparent";
+    btnRegistroForm.style.color= "black";
 
 }
