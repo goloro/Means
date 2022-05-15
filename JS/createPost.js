4533f534bf6d7571b35c36a55ece3cb501a92061
Gonzalo <goloro02@gmail.com>
2022-05-15 20:50:13 +0200

    Finish createPost

--- JS/createPost.js
+++ JS/createPost.js
@@ -4,6 +4,9 @@ import { createPostCall } from './common/dbCalls/posts.js'
 // CONSTS
 const localUser = JSON.parse(localStorage.getItem('Means_userLogued'))
 
+const problemCard = document.querySelector(".problemCard")
+const msgProblem = document.getElementById("problemMsg")
+
 // EVENT LISTENERS
 document.getElementById("btnCP").addEventListener("click", e => {
     e.preventDefault()
@@ -23,12 +26,25 @@ document.getElementById("peopleField").addEventListener("click", e => {
     }
     if (targetId === "addPeopleLine") {
         const divPeopleLines = document.getElementById("divPeopleLines")
-        divPeopleLines.innerHTML += `
-        <div id="${divPeopleLines.childElementCount}">
-            <input id="tipo${divPeopleLines.childElementCount}" class="leftItemPeopleTable" type="text">
-            <input id="number${divPeopleLines.childElementCount}" class="rigthItemPeopleTable" type="number">
-        </div>
+        const peopleLine = document.createElement("div")
+        let lines = divPeopleLines.childElementCount
+        peopleLine.id = lines
+        peopleLine.innerHTML += `
+            <img id="delete${lines}" class="${lines}" src="https://api.iconify.design/akar-icons/trash-can.svg?color=%23fb5050">
+            <input id="tipo${lines}" class="leftItemPeopleTable" type="text">
+            <input id="number${lines}" class="rigthItemPeopleTable" type="number">
         `
+        divPeopleLines.appendChild(peopleLine)
+    }
+
+    if (e.target.tagName === "IMG" && e.target.className !== "addLine") {
+        divPeopleLines.removeChild(document.getElementById(e.target.className))
+        let cont = 
+        divPeopleLines.childNodes.forEach(e => {
+            e.id = cont
+            cont++
+        });
+        cont = 0
     }
 })
 document.getElementById("moneyButtons").addEventListener("click", e => {
@@ -42,6 +58,34 @@ document.getElementById("moneyButtons").addEventListener("click", e => {
     if (targetId === "moneyNo") {
         moneyInput.style.display = "none"
         changeColorNo(document.getElementById("moneyYes"), document.getElementById("moneyNo"))
+    }
+})
+let previousIMGCat
+let selectedIMGCat
+document.getElementById("imgCatCP").addEventListener("click", e => {
+    let target = e.target
+    if (target.tagName === "IMG") {
+        if (previousIMGCat) {
+            if (previousIMGCat.src != target.src) {
+                previousIMGCat.style.border = "1px solid transparent"
+                previousIMGCat.style.boxShadow = "none"
+
+                target.style.border = "1px solid black"
+                target.style.boxShadow = "2px 2px 10px 1px rgba(0, 0, 0, 0.2)"
+
+                previousIMGCat = target
+            } else {
+                target.style.border = "1px solid transparent"
+                target.style.boxShadow = "none" 
+                previousIMGCat = null
+            }
+        } else {
+            target.style.border = "1px solid black"
+            target.style.boxShadow = "2px 2px 10px 1px rgba(0, 0, 0, 0.2)"
+
+            previousIMGCat = target
+        }
+        selectedIMGCat = target.src
     }
 })
 
@@ -59,19 +103,81 @@ function createPost() {
     const name = document.getElementById("namePost")
     const descShort = document.getElementById("descShortPost")
     const descLarge = document.getElementById("descLargePost")
-    const money = document.getElementById("moneyPost")
     const startDate = document.getElementById("startDatePost")
     const finishDate = document.getElementById("finishDatePost")
+    const money = document.getElementById("moneyPost")
+    const people = []
 
-    createPostCall({
+    if (!name.value) {
+        createProblem("Falta ponerle un nombre a tu post!")
+        return
+    }
+    if (!descShort.value) {
+        createProblem("Falta ponerle una descripción breve a tu post!")
+        return
+    }
+    if (!descLarge.value) {
+        createProblem("Falta ponerle una descripción extensa a tu post!")
+        return
+    }
+    if (!startDate.value) {
+        createProblem("Falta ponerle una fecha de inicio a tu post!")
+        return
+    }
+    if (!finishDate.value) {
+        createProblem("Falta ponerle una fecha de fin a tu post!")
+        return
+    }
+    if (!selectedIMGCat) {
+        createProblem("Debes elegir una imagen para tu post!")
+        return
+    }
+    if (document.getElementById("peopleTable").style.display != "flex" && document.getElementById("moneyPost").style.display != "flex") {
+        createProblem("Debes solicitar dinero o personas para tu post!")
+        return
+    }
+
+    const postDoc = {
         idUser: localUser.email,
         name: name,
         smallDescription: descShort,
         largeDescription: descLarge,
-        money: money,
         startDate: startDate,
         finishDare: finishDate,
-    })
+        image: selectedIMGCat,
+    }
+
+    let peopleFlag = false
+    if (document.getElementById("peopleTable").style.display == "flex") {
+        document.getElementById("divPeopleLines").childNodes.forEach(e => {
+            if (e.tagName === "DIV") {
+                let tipo = document.getElementById("tipo"+e.id).value
+                let count = document.getElementById("number"+e.id).value
+                if (tipo == "" || count == "" || tipo == " " || count == " " || count == 0) peopleFlag = true
+                people.push({
+                    type: tipo,
+                    count: count
+                })
+            }
+        })
+        postDoc.people = people
+    }
+    if (peopleFlag) {
+        createProblem("Rellena el tipo y el número de las personas solicitadas!")
+        return
+    }
+    let moneyFlag = false
+    if (document.getElementById("moneyPost").style.display == "flex") {
+        if (money.value == "" || money.value == "" || money.value == 0) moneyFlag = true
+        postDoc.money = money.value
+    }
+    if (moneyFlag) {
+        createProblem("Debes introducir una cantidad mayor a 0 en el dinero solicitado!")
+        return
+    }
+
+    problemCard.style.visibility = "hidden"
+    createPostCall(postDoc)
 
     name.value = ""
     descShort.value = ""
@@ -79,4 +185,9 @@ function createPost() {
     money.value = ""
     startDate.value = ""
     finishDate.value = ""
+}
+
+function createProblem(msg) {
+    msgProblem.innerHTML = msg
+    problemCard.style.visibility = "visible"
 }
